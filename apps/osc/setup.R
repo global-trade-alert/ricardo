e03d845fc203ce778a1b65383734d8f2c76030d4
patrickbuess <mail@patrickbuess.ch>
2020-04-22 10:40:57 +0200

    adjust osc/setup.R

--- apps/osc/setup.R
+++ apps/osc/setup.R
@@ -1,6 +1,3 @@
-# IMPORT MODULES
-source(paste0(path,"apps/osc/modules/pop_up.R"))
-
 # Import osc functions
 for(fct in list.files(paste0(path,"apps/osc/functions"), pattern = ".R", full.names=T)){
   source(fct)
