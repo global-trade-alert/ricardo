344381270c1e6d417f35bd92a5544291bf36a4b3
patrickbuess <mail@patrickbuess.ch>
2020-06-03 14:43:42 +0200

    merge with current master

--- www/b221/app.js
+++ www/b221/app.js
@@ -151,8 +151,9 @@ function slideInBasicUI() {
 }
 
 function markHints() {
+  console.log("markHints loaded");
   
-  $('#b221-slideInRight #hintContainer').on('click','.hint-title', function (e) {
+  $('#b221-slideInRight').on('click','#hintContainer .hint-title', function (e) {
     console.log(e);
     if(e.target.className == 'hint-title') {
     if ($(this.parentNode.parentNode).hasClass('starred')) {
