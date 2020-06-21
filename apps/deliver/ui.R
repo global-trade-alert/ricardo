4c4a236608cd05d1179c892faf31ba2934d47d34
Patrick Buess <mail@patrickbuess.ch>
2020-06-21 14:16:57 +0200

    Styling Editmode, improve main ricardo menu behaviour

--- apps/deliver/ui.R
+++ apps/deliver/ui.R
@@ -1,7 +1,3 @@
-# addResourcePath(prefix = 'www', directoryPath = paste0(path,'apps/b221/www'))
-
- addResourcePath(prefix = 'www', directoryPath = paste0(path,'www/deliver'))
-
 deliverui <- function(id) {
   # Create a namespace function using the provided id
   ns <- NS(id)
@@ -25,7 +21,6 @@ deliverui <- function(id) {
                                tags$div(class="deliverTable",
                                         dataTableOutput(ns("deliverTable"))) # Initialize selectize),
                       ),
-                      tags$div(id="searchPanes"),
                       tags$div(class="control-bar")
 
              )
