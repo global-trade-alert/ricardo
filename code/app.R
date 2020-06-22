f3285ae3bbf2fb4d1d3adf16973c8485bcafae51
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2020-06-22 10:02:29 +0200

    initial commit

--- code/app.R
+++ code/app.R
@@ -10,7 +10,7 @@ gtasql::gta_sql_kill_connections()
 gta_setwd()
 # setwd("/Users/patrickbuess/GTA data team Dropbox/GTA cloud")
 # path <<- "17 Shiny/8 ricardo app/"
-path <<- "0 dev/ricardo-pb/"
+path <<- "0 dev/ricardo-discard/"
 
 # APP SETUP
 source(paste0(path,"code/setup.R"), local = F)
