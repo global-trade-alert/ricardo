dc7d183f53a03d0e0763b5fb2ccbbd4c8695664c
Johannes Fritz <fritz.johannes@gmail.com>
2020-09-16 21:21:06 +0200

    adding product groups to priority list

--- code/functions/bt_attribute_hint_processing.R
+++ code/functions/bt_attribute_hint_processing.R
@@ -14,17 +14,19 @@ bt_attribute_hint_processing = function(user.id = NULL, hint.state = NULL, db.co
                           JOIN (SELECT jurisdiction_id FROM ric_user_implementers WHERE user_id = ",user.id," AND app_id = ",app.id,") user_prio_jur ON ht_jur.jurisdiction_id = user_prio_jur.jurisdiction_id
                           LEFT JOIN b221_hint_intervention ON b221_hint_intervention.hint_id = bt_hint_log.hint_id
                           JOIN bt_hint_text ON bt_hint_text.hint_id = bt_hint_log.hint_id AND bt_hint_text.hint_description IS NOT NULL AND bt_hint_text.hint_description != '' AND bt_hint_text.hint_title IS NOT NULL AND bt_hint_text.hint_title != ''
+                          LEFT JOIN b221_hint_product_group ON bt_hint_log.hint_id = b221_hint_product_group.hint_id
                           JOIN bt_hint_state_list ON bt_hint_log.hint_state_id = bt_hint_state_list.hint_state_id AND (bt_hint_state_list.hint_state_name = ",hint.state,") 
                           AND NOT EXISTS (SELECT NULL FROM bt_hint_processing WHERE bt_hint_log.hint_id = bt_hint_processing.hint_id) 
-                          ORDER BY FIND_IN_SET(apparent_intervention_id, '2,3') DESC, bt_hint_log.registration_date DESC LIMIT 10) prio_hints
+                          ORDER BY FIND_IN_SET(product_group_id, '2,3,4,5') DESC, FIND_IN_SET(apparent_intervention_id, '2,3') DESC, bt_hint_log.registration_date DESC LIMIT 10) prio_hints
                           UNION 
                           SELECT hint_id FROM
                           (SELECT bt_hint_log.hint_id FROM bt_hint_log 
                           LEFT JOIN b221_hint_intervention ON b221_hint_intervention.hint_id = bt_hint_log.hint_id
                           JOIN bt_hint_text ON bt_hint_text.hint_id = bt_hint_log.hint_id AND bt_hint_text.hint_description IS NOT NULL AND bt_hint_text.hint_description != '' AND bt_hint_text.hint_title IS NOT NULL AND bt_hint_text.hint_title != ''
+                          LEFT JOIN b221_hint_product_group ON bt_hint_log.hint_id = b221_hint_product_group.hint_id
                           JOIN bt_hint_state_list ON bt_hint_log.hint_state_id = bt_hint_state_list.hint_state_id AND (bt_hint_state_list.hint_state_name = ",hint.state,") 
                           AND NOT EXISTS (SELECT NULL FROM bt_hint_processing WHERE bt_hint_log.hint_id = bt_hint_processing.hint_id)
-                          ORDER BY FIND_IN_SET(apparent_intervention_id, '2,3') DESC, bt_hint_log.registration_date DESC LIMIT 10) non_prio_hints) hints LIMIT 10;")
+                          ORDER BY FIND_IN_SET(product_group_id, '2,3,4,5') DESC, FIND_IN_SET(apparent_intervention_id, '2,3') DESC, bt_hint_log.registration_date DESC LIMIT 10) non_prio_hints) hints LIMIT 10;")
   
   hints <<- na.omit(data.frame(hint.id = gta_sql_get_value(sql.pull.hints), user.id = user.id, start.time = substr(as.POSIXct(Sys.time(), tz = "CET"),1,19)))
 
